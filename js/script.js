3ebd1b753b7505d6ee8524c0b64695e62a361b5f
acharygirish <acharygirish@gmail.com>
2023-01-23 17:35:26 +0530

    modified files

--- js/script.js
+++ js/script.js
@@ -1,89 +1,62 @@
 fetch('https://randomuser.me/api')
-.then((res)=>{
+.then(function(res){
     data = res.json();
     return data;
 })
 .then(function(data){
     console.log(data);
-    var personalData = data.results[0];
-    var img = personalData.picture.large;
-    var name = personalData.name.first+" "+personalData.name.last;
-    var email = personalData.email;
-    var dob = new Date(personalData.dob.date);
-    let day = dob.getDate();
-    let month = dob.getMonth();
-    let year = dob.getFullYear();
-    var add = personalData.location.street.number+" "+personalData.location.street.name;
-    var phone = personalData.phone;
-    var pass = personalData.login.password;
-    document.getElementById('email').innerHTML = email;
-    document.getElementById('dob').innerHTML = day+'/'+month+'/'+year;
-    document.getElementById('add').innerHTML = add;
-    document.getElementById('phone').innerHTML = phone;
-    document.getElementById('pass').innerHTML = pass;
-    document.getElementById('img').src = img;
-    document.getElementById('name').innerHTML = name;
+    personalData = data.results[0];
+    var profileImg = personalData.picture.large;
+    var fullName = '<h3>Hi, My Name is </h3><h1>'+personalData.name.first + " " + personalData.name.last+'</h1>';
+    document.querySelector('#content').innerHTML = fullName;
+    document.querySelector('#userImg').src = profileImg;
 })
 .catch(function(error){
-    console.log(error);
+    error = 'API not found';
+    alert(error);
 });
 
-function showData(){
-    var name = document.getElementById('name')
-    if(name.style.visibility == 'visible')
-    {
-        name.style.visibility = 'hidden';
-    }
-    else{
-        name.style.visibility = 'visible';
-    }
-}
-function showEmail(){
-    var email = document.getElementById('email')
-    if(email.style.visibility == 'visible')
-    {
-        email.style.visibility = 'hidden';
-    }
-    else{
-        email.style.visibility = 'visible';
-    }
-}
-function showDob(){
-    var dob = document.getElementById('dob')
-    if(dob.style.visibility == 'visible')
-    {
-        dob.style.visibility = 'hidden';
-    }
-    else{
-        dob.style.visibility = 'visible';
-    }
-}
-function showAdd(){
-    var add = document.getElementById('add')
-    if(add.style.visibility == 'visible')
-    {
-        add.style.visibility = 'hidden';
-    }
-    else{
-        add.style.visibility = 'visible';
-    }
-}function showPhone(){
-    var phone = document.getElementById('phone')
-    if(phone.style.visibility == 'visible')
-    {
-        phone.style.visibility = 'hidden';
-    }
-    else{
-        phone.style.visibility = 'visible';
-    }
-}
-function showPass(){
-    var pass = document.getElementById('pass')
-    if(pass.style.visibility == 'visible')
-    {
-        pass.style.visibility = 'hidden';
-    }
-    else{
-        pass.style.visibility = 'visible';
-    }
-}
+document.getElementById('user1').addEventListener('mouseover',function(data)
+{
+    var fullName = personalData.name.first + " " + personalData.name.last;
+    var content = document.querySelector('#content');
+    content.innerHTML ='<h3>Hi, My Name is </h3><h1>'+fullName+'</h1>';
+})
+
+document.querySelector('#user2').addEventListener('mouseover',function(data)
+{
+    var email = personalData.email;
+    var content = document.querySelector('#content');
+    content.innerHTML ='<h3>My Email-id is </h3><h1>'+email+'</h1>';
+})
+
+document.querySelector('#user3').addEventListener('mouseover',function(data)
+{
+    var dob = new Date(personalData.dob.date);
+    day = dob.getDate();
+    month = dob.getMonth();
+    year = dob.getFullYear();
+    var content = document.querySelector('#content');
+    content.innerHTML ='<h3>Born in </h3><h1>'+day+"/"+month+"/"+year+'<h1>';
+})
+
+document.querySelector('#user4').addEventListener('mouseover',function(data)
+{
+    var add = personalData.location.street.name+", "+personalData.location.state+", "+personalData.location.country+", "+personalData.location.postcode;
+    var content = document.querySelector('#content');
+    content.innerHTML ='<h3>My Location is </h3><h1>'+add+'</h1>';
+})
+
+document.querySelector('#user5').addEventListener('mouseover',function(data)
+{
+    var phone = personalData.phone;
+    var content = document.querySelector('#content');
+    content.innerHTML ='<h3>My Contact Number is </h3><h1>'+phone+'</h1>';
+})
+
+document.querySelector('#user6').addEventListener('mouseover',function(data)
+{
+    var pass = personalData.login.password;
+    var content = document.querySelector('#content');
+    content.innerHTML ='<h3>My Password is </h3><h1>'+pass+'</h1>';
+})
